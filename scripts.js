e468822ce8fe4bd1b41f6661cfee9fa970bc4fb3
endaevor <101499715+ndaevor@users.noreply.github.com>
2024-08-16 16:35:56 +0530

    mmmm

--- scripts.js
+++ scripts.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const password = document.getElementById('password').value;
 
             try {
-                const response = await fetch('http://localhost:5000/api/users/register', {
+                const response = await fetch('https://whitematrix-1.onrender.com/api/users/register', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ name, email, password })
